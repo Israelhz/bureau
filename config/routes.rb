dc1a892aa4115ea73268ae17067bcf8d4f5ad905
israelhz <israel.leal25@gmail.com>
2018-04-02 12:46:24 -0500

    Add engine

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  mount Bureau::Engine => "/bureau"
+end
--- lib/bureau.rb
+++ lib/bureau.rb
@@ -1,5 +1,10 @@
 require "bureau/version"
+require "bureau/engine"
 
 module Bureau
-  # Your code goes here...
+  class InstallGenerator < Rails::Generators::Base
+    def create_initializer_file
+      create_file "config/initializers/bureau.rb", "# Add initialization content here"
+    end
+  end
 end
